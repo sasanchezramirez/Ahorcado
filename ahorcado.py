dfa864f57515ad90e14ae2661f5692e0e7e1853d
Santiago Sánchez <sasanchezramirez@gmail.com>
2022-08-01 09:46:47 -0500

    ahora puedo encontrar si hay match de las letras ingresadas

--- ahorcado.py
+++ ahorcado.py
@@ -8,13 +8,25 @@ def bring_line():
             list_lines.append(line)
     return list_lines[line_number] 
 
-def guiones (palabra):
-    num_guiones = list(enumerate(palabra))
-    return num_guiones
+def comparision (palabra):
+    letras = []
+    for index, letra in enumerate(palabra):
+        letras.append(letra)
+    letras.pop(len(letras)-1)
+    print("_ "*len(letras))
+
+    user_input = input("ingrese una letra")
+    for index, letra in enumerate(palabra):
+        if user_input == letra:
+            print("hay un match en ", index)
+    return letras    
+    # num_guiones = list(enumerate(palabra))
+    # for letra in num_guiones:
+    #     return num_guiones
 
 def run():
     palabra = bring_line()
-    num_guiones = guiones(palabra)
+    num_guiones = comparision(palabra)
     print(num_guiones)
 
 
